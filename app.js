faabc6117d9d1763a77460dc788e16088f0f60b7
charlesmartinreed <charlesmartinreed@icloud.com>
2019-12-12 19:02:11 -0600

    Integrate client side stripe checkout. Create success.html for completed purchases.

--- app.js
+++ app.js
@@ -5,7 +5,21 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 
+const keyPublic = process.env.STRIPE_PUBLIC_KEY;
+const keyPrivate = process.env.STRIPE_PRIVATE_KEY;
+
+const stripe = require("stripe")(keyPrivate);
+
+// VIEW ENGINE
+// app.set("view engine", "ejs");
+
+// MIDDLEWARE
+app.use(express.json());
 app.use(express.static("public"));
+
+// ROUTES
+
+// START SERVER
 
 app.listen(4500, () => {
   console.log("app is now running");
